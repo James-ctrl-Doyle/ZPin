8908160cfe69055e3636fb7dff11dd92f1ee7d9b
James-ctrl-Doyle <67906228+James-ctrl-Doyle@users.noreply.github.com>
2026-09-20 21:36:13 +0800

    保存文件名与截图历史格式统一：YYYYMMDD_HHMMSS_mmm
    Util::createFileName 原来是无分隔的 YYYYMMDDHHMMSSmmm，改成与 temp/shots 截图历史一致的 日期_时间_毫秒（如 20260920_150837_330）。保存的 png 和录屏 mp4 都走这一个函数，一并生效。

--- Src/Util.cpp
+++ Src/Util.cpp
@@ -561,7 +561,8 @@ std::wstring Util::createFileName(const std::wstring& ext)
 {
 	SYSTEMTIME st;
 	GetLocalTime(&st);
-	return std::format(L"{:04d}{:02d}{:02d}{:02d}{:02d}{:02d}{:03d}.{}",
+	// 与截图历史（temp\shots）同一套命名：日期_时间_毫秒，中间用下划线分隔
+	return std::format(L"{:04d}{:02d}{:02d}_{:02d}{:02d}{:02d}_{:03d}.{}",
 		st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds, ext);
 }
 
